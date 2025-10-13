dab17c569dd47e2018b9bff9edfa2b471a551d58
vickycastanio <vcastanoseippel@udesa.edu.ar>
2025-10-13 16:31:48 -0300

    segui guia

--- clase09/practica3parte2/App.js
+++ clase09/practica3parte2/App.js
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import ProductsAll from './src/Screens/ProductsAll';
+import { StyleSheet, View } from "react-native";
+import ProductsAll from "./src/Screens/ProductsAll";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductsAll/>
+      <ProductsAll />
     </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
